8a04c348d0d181b18624fb9b077d2c623b95991d
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-05-16 17:50:23 +0530

    Add margin in Home page root

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import { Box, Container, Typography } from '../components/Mui/material';
 
 export default function RootPage() {
   return (
-    <Container>
+    <Container maxWidth='xl' sx={{ marginLeft: '30rem' }}>
       <Typography variant='h1' justifyContent='center' alignItems='center'>
         Please Select Course
       </Typography>
